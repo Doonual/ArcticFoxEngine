41c8e71d59e0ab264f13a30bef53b362d05d20f0
Doonual <doonual@gmail.com>
2025-04-03 02:34:13 +0800

    Fixed a bug with loading the opened GUI windows
    Changed an i to an n
    Its the little things that will get you

--- Gui/GuiManager.cs
+++ Gui/GuiManager.cs
@@ -420,7 +420,8 @@ namespace ArcticFoxEngine.Gui {
 				// Open target window
 				for (int n = 0; n < possibleWindowsList.Count; n ++) {
 					if (GetNameOfGuiWindow(possibleWindowsList[n]) == name) {
-						OpenGuiWindow((GuiWindow)Activator.CreateInstance(possibleWindowsList[i]));
+						Log.Info("Found match with " + name);
+						OpenGuiWindow((GuiWindow)Activator.CreateInstance(possibleWindowsList[n]));
 					}
 				}
 
